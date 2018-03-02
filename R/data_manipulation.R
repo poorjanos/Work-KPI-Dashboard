60007da9c786ea2da47a6f28751207ed925de670
Janos Poor <poorjanos@gmail.com>
2018-03-02 08:34:23 +0100

    Set up .Rmd with proper paths

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -6,11 +6,14 @@ readQuery <-
 
 # Function to write daily query results to log on local storage
 write_to_log <- function(df_curr, path_to_log) {
-  # Initialize curr as hist if folder empty
+  # Initialize curr as hist if folder empty and enough data in curr
   if (!file.exists(path_to_log) & nrow(df_curr) > 100) {
     df_hist <- df_curr
     write.csv(df_hist, path_to_log, row.names = FALSE)
     return(df_hist)
+    # Exit if folder empty but not enough data in curr
+  } else if (!file.exists(path_to_log) & nrow(df_curr) < 100) {
+    return()
     # Check further if folder not empty
   } else {
     df_hist <-
@@ -36,7 +39,7 @@ write_to_log <- function(df_curr, path_to_log) {
 }
 
 
-# Function to generate KPI aggregates
+# Function to generate KPI aggregates: AL
 generate_table <- function(tabla, ...) {
   tbl_df(tabla) %>%
     group_by_(...) %>%
@@ -56,7 +59,7 @@ generate_table <- function(tabla, ...) {
 }
 
 
-# Function to generate KPI aggregates
+# Function to generate KPI aggregates: AL
 generate_table_15 <- function(tabla, ...) {
   tbl_df(tabla) %>%
     group_by_(...) %>%
@@ -71,7 +74,7 @@ generate_table_15 <- function(tabla, ...) {
 }
 
 
-# Function to generate KPI aggregates
+# Function to generate KPI aggregates: IL
 generate_table_kontakt <- function(tabla, ...) {
   tbl_df(tabla) %>%
     group_by_(...) %>%
@@ -89,7 +92,7 @@ generate_table_kontakt <- function(tabla, ...) {
 }
 
 
-# Function to generate KPI aggregates
+# Function to generate KPI aggregates: IL
 generate_table_15_kontakt <- function(tabla, ...) {
   tbl_df(tabla) %>%
     group_by_(...) %>%
@@ -102,3 +105,22 @@ generate_table_15_kontakt <- function(tabla, ...) {
     group_by_(...) %>% # bring stacked bar text to middle position
     mutate(POS=cumsum(NAGYOBB_15_NAP)-(0.5*NAGYOBB_15_NAP)) # bring stacked bar text to middle position
 }
+
+
+# Function to generate KPI aggregates: PU
+generate_table_pu <- function(tabla, ...)
+    {
+      tbl_df(tabla) %>%
+        group_by_(...) %>%
+        summarize(DARAB = length(SORSZAM),
+                  ERK_KONYV_ATLAG = round(mean(ERK_KONYV), 2),
+                  ERK_KONYV_MEDIAN = round(median(ERK_KONYV), 2),
+                  ERK_KONYV_SD = round(sd(ERK_KONYV), 2),
+                  ERK_KONYV_MAD = round(mad(ERK_KONYV), 2)) %>%
+        ungroup() %>% #weight within monthly volume
+        group_by_("DATUM") %>% #weight within monthly volume
+        mutate(DB_SULY = round(DARAB/sum(DARAB), 4)) %>% #weight within monthly volume
+        ungroup() %>% # bring stacked bar text to middle position
+        group_by_(...) %>% # bring stacked bar text to middle position
+        mutate(POS=cumsum(ERK_KONYV_ATLAG)-(0.5*ERK_KONYV_ATLAG)) # bring stacked bar text to middle position
+    }
